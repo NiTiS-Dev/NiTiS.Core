8267f72705a32ad7ab688d34f7013c67f26158a2
NiTiSon <NiTiSon@users.noreply.github.com>
2022-11-25 21:35:23 +0300

    A

---
+++ NiTiS.Core/Annotations/NotImplementYetAttribute.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace NiTiS.Core.Annotations;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Constructor | AttributeTargets.Property | AttributeTargets.Event | AttributeTargets.Interface)]
+public sealed class NotImplementYetAttribute : Attribute { }
---
+++ NiTiS.Core/Operators/ICastOperators.cs
@@ -0,0 +1,11 @@
+﻿#if NET7_0_OR_GREATER
+namespace NiTiS.Core.Operators;
+public interface IExplicitCastOperators<TSelf, TOther> where TSelf : IExplicitCastOperators<TSelf, TOther>
+{
+	static abstract explicit operator TOther(TSelf self);
+}
+public interface IImplicitCastOperators<TSelf, TOther> where TSelf : IImplicitCastOperators<TSelf, TOther>
+{
+	static abstract implicit operator TOther(TSelf self);
+}
+#endif
