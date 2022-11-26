5872224950f54ff590796b2d4e834d20f40b6a00
NiTiSon <NiTiSon@users.noreply.github.com>
2022-11-27 00:07:58 +0300

    Move math to other package

---
+++ NiTiS.Core/Annotations/ObsoleteImplAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NiTiS.Core.Annotations;
+
+[AttributeUsage(
+	  AttributeTargets.Class
+	| AttributeTargets.Struct
+	| AttributeTargets.Enum
+	| AttributeTargets.Method
+	| AttributeTargets.Property
+	| AttributeTargets.Event
+	| AttributeTargets.Interface
+	)]
+public class ObsoleteImplAttribute : Attribute { }
---
+++ NiTiS.Core/Annotations/RenameRequestAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NiTiS.Core.Annotations;
+
+[AttributeUsage(
+	  AttributeTargets.Field
+	| AttributeTargets.Class
+	| AttributeTargets.Struct
+	| AttributeTargets.Enum
+	| AttributeTargets.Method
+	| AttributeTargets.Property
+	| AttributeTargets.Field
+	| AttributeTargets.Interface
+	| AttributeTargets.Parameter
+	| AttributeTargets.Delegate
+	)]
+public sealed class RenameRequestAttribute : Attribute { }
